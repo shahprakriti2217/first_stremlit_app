0ad839b8e0f108a0def0837a0396bccbfb6321b5
shahprakriti2217 <134002102+shahprakriti2217@users.noreply.github.com>
2023-05-19 15:26:31 +0530

    Update streamlit_app.py
    importing pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.header('this is the header of the app')
 streamlit.text('I want to make this app')
 streamlit.text('🥣soup')
 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
