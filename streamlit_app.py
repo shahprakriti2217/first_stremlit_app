d977325e5c493547f7c47d9fa761c824117a1181
shahprakriti2217 <134002102+shahprakriti2217@users.noreply.github.com>
2023-05-19 15:27:02 +0530

    Update streamlit_app.py
    adding a data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ streamlit.text('🥣soup')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
